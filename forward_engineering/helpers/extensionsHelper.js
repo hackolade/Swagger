d1ddab4ee91905d76c563e5d95dc124012c050b6
Pascal Desmarets <pascal@desmarets.org>
2019-12-23 09:00:06 +0100

    Fix complex extensions handling

--- forward_engineering/helpers/extensionsHelper.js
+++ forward_engineering/helpers/extensionsHelper.js
@@ -2,8 +2,15 @@ function getExtensions(data = []) {
     return data
         .filter(filterExtensionsByPrefix)
         .reduce((acc, { extensionPattern, extensionValue }) => {
-            acc[extensionPattern] = extensionValue;
-            return acc;
+			let parsedValue;
+			try {
+				parsedValue = JSON.parse(extensionValue);
+			} catch(err) {
+				parsedValue = extensionValue;
+			}
+			acc[extensionPattern] = parsedValue;
+
+			return acc;
         }, {});
 
  }
