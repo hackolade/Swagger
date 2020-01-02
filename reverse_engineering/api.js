64d7781f0e1636cba43aac2c111fa52065bec7ea
mtseluiko <mikhail.tseluiko@binary-studio.com>
2020-01-02 16:33:56 +0200

    fix merge conflict

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -5,6 +5,7 @@ const dataHelper = require('./helpers/dataHelper');
 const errorHelper = require('./helpers/errorHelper');
 const adaptJsonSchema = require('./helpers/adaptJsonSchema/adaptJsonSchema');
 const validationHelper = require('../forward_engineering/helpers/validationHelper');
+const resolveExternalDefinitionPathHelper = require('./helpers/resolveExternalDefinitionPathHelper');
 
 module.exports = {
 	reFromFile(data, logger, callback) {
@@ -61,7 +62,11 @@ module.exports = {
         } catch(e) {
             callback(commonHelper.handleErrorObject(e, 'Adapt JSON Schema'), data);
         }
-    }
+    },
+
+	resolveExternalDefinitionPath(data, logger, callback) {
+		resolveExternalDefinitionPathHelper.resolvePath(data, callback);
+	}
 };
 
 const convertSwaggerSchemaToHackolade = (swaggerSchema, fieldOrder) => {
@@ -72,7 +77,7 @@ const convertSwaggerSchemaToHackolade = (swaggerSchema, fieldOrder) => {
 };
 
 const getSwaggerSchema = (data, filePath) => new Promise((resolve, reject) => {
-    const { extension, fileName } = commonHelper.getPathData(filePath);
+    const { extension, fileName } = commonHelper.getPathData(data, filePath);
 
     try {
         const swaggerSchemaWithModelName = dataHelper.getSwaggerJsonSchema(data, fileName, extension);
